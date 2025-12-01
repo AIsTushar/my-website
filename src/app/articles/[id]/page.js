d3d9505a3d0e1bfc3b90b4b602537ef67bb1f1bb
AIsTushar <azizultushar98@gmail.com>
2025-12-01 15:38:17 +0600

    changed few things in the airticale page.

--- src/app/articles/[id]/page.js
+++ src/app/articles/[id]/page.js
@@ -30,25 +30,30 @@ export default function Page() {
 
       {/* CONTENT SECTIONS THAT SCROLL OVER THE IMAGE */}
       <div className="relative z-10 bg-slate-900">
-        <section className="flex min-h-screen items-center p-10">
-          <div>
-            <h1 className="mb-4 text-4xl font-bold">
-              Entry Level React Interview Questions
-            </h1>
-            <p>
-              10 questions and relatively short but comprehensive answers for
-              entry-level devs.
+        <div className="mx-auto flex min-h-screen flex-col">
+          <p className="max-w-lg text-left text-sm text-gray-400">
+            Just ran my first race ever—the San Francisco Half Marathon. The
+            experience? Surprisingly similar to building a startup. Here&lsquo;s
+            what I learned...
+          </p>
+          <section className="mx-auto flex min-h-screen max-w-4xl flex-col gap-12 p-10">
+            <h2 className="text-left text-3xl text-gray-50">
+              1. Unfavorable conditions
+            </h2>
+
+            <p className="text-gray-100">
+              When I woke up for race day, it was raining like crazy. By the
+              time I arrived at Golden Gate Park, it was still raining. During
+              the race, the rain got even worse. That&lsquo;s how it feels when
+              you start your own company. You might be too late, or maybe too
+              early. The market might pull you into a different direction or
+              your users may not like your product at first. You might need to
+              pivot, or even start from the beginning again. Whatever happens,
+              conditions are never going to be perfect. Don&lsquo;t expect a
+              sunny day.
             </p>
-          </div>
-        </section>
-
-        <section className="flex min-h-screen items-center p-10">
-          <p>Second section content goes here...</p>
-        </section>
-
-        <section className="flex min-h-screen items-center p-10">
-          <p>Third section content goes here...</p>
-        </section>
+          </section>
+        </div>
       </div>
     </div>
   );
